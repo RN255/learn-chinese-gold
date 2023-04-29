7cb3c818dd2f9e04bb64e72afc66594ae9b55b34
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-04-29 22:44:09 +0100

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+      <h1>Welcome to the homepage!</h1>
+      <p>This is some example text for the homepage.</p>
+    </>
+  );
+};
+
+export default Home;
