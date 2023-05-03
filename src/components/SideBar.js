9aed3f66a042af70d9de3db00ca6d2e9120a77ea
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-05-03 14:54:26 +0100

    prev next buttons complete

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -13,32 +13,55 @@ export default function SideBar(props) {
   const prevLessonNum = parseInt(props.LessonText.lessonNumber) - 1;
 
   return (
-    <div className="container">
-      {typeof nextLessonNum}
-
-      <p>{props.LessonText.lessonNumber}</p>
-
-      <Button variant="primary" disabled={prevLessonNum < 1 ? true : false}>
-        <Link
-          to={`/lesson${prevLessonNum}`}
-          className="text-decoration-none text-light"
-        >
-          Previous Lesson
-        </Link>
-      </Button>
-
-      <Button variant="primary" onClick={handleShow}>
-        Lesson Index
-      </Button>
-
-      <Button variant="primary" disabled={nextLessonNum > 5 ? true : false}>
-        <Link
-          to={`/lesson${nextLessonNum}`}
-          className="text-decoration-none text-light"
-        >
-          Next Lesson
-        </Link>
-      </Button>
+    <div className="container sideBar">
+      <div className="row d-flex flex-row justify-content-center">
+        <div className="col col-lg-2 col-xl-1 text-center p-0">
+          {prevLessonNum < 1 ? (
+            <Button
+              variant="link"
+              size="sm"
+              className="lessonSelectButtons text-decoration-none"
+              disabled
+            >
+              &lt; Previous
+            </Button>
+          ) : (
+            <Link className="nextPrevLink" to={`/lesson${prevLessonNum}`}>
+              <Button variant="link" size="sm" className="lessonSelectButtons">
+                &lt; Previous
+              </Button>
+            </Link>
+          )}
+        </div>
+        <div className="col col-lg-2 col-xl-2 text-center ">
+          <Button
+            variant="link"
+            size="sm"
+            className="lessonsIndexButton"
+            onClick={handleShow}
+          >
+            Lesson Index
+          </Button>
+        </div>
+        <div className="col col-lg-2 col-xl-1 text-center p-0">
+          {nextLessonNum > 5 ? (
+            <Button
+              variant="link"
+              size="sm"
+              className="text-decoration-none"
+              disabled
+            >
+              Next &gt;
+            </Button>
+          ) : (
+            <Link to={`/lesson${nextLessonNum}`}>
+              <Button variant="link" size="sm" className="lessonSelectButtons">
+                Next &gt;
+              </Button>
+            </Link>
+          )}
+        </div>
+      </div>
 
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
