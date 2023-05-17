3f42688d97c5effd21449094d1e62765a3e83c9d
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-05-17 15:40:30 +0100

    footer spacing improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,14 @@ export default function Footer() {
   return (
     <>
       <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <p className="col-md-4 mb-0 text-body-secondary">© 2023 Let's Learn Chinese</p>
+        <footer className="d-md-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p className="col-md-4 mb-0 text-center text-md-start">
+            © 2023 Let's Learn Chinese
+          </p>
 
-          <ul className="nav col-md-4 justify-content-end">
+          <ul className="nav col-md-4 justify-content-center justify-content-md-end">
             <li className="nav-item">
-              <Nav.Link
-                className="nav-link px-2 text-body-secondary footerLink"
-                href="/"
-                to="/"
-              >
+              <Nav.Link className="nav-link px-2 footerLink" href="/" to="/">
                 {" "}
                 Home{" "}
               </Nav.Link>
