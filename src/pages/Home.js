057d5f0f910ef6fccfd71535ac6bc5991c8e169e
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-05-18 20:36:21 +0100

    audio elements removed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import boyImg from "../assets/images/boy.png";
 import girlImg from "../assets/images/girl.png";
 import MainConvo from "../components/MainConvo";
 import TranslatedWords from "../components/TranslatedWords";
-import Audio from "../components/Audio";
+// import Audio from "../components/Audio";
 import KeyPhrases from "../components/KeyPhrases";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -78,7 +78,7 @@ const Home = (props) => {
 
       {/* <TitleSection LessonText={props.LessonText}></TitleSection> */}
 
-      <Audio LessonText={props.LessonText} Flashing={true}></Audio>
+      {/* <Audio LessonText={props.LessonText} Flashing={true}></Audio> */}
 
       <div className="container homeItemCustomMargin">
         <div className="row text-center">
--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -4,7 +4,7 @@ import TitleSection from "../components/TitleSection";
 import MainConvo from "../components/MainConvo";
 import MainConvoCustom from "../components/MainConvoCustom";
 import TranslatedWords from "../components/TranslatedWords";
-import Audio from "../components/Audio";
+// import Audio from "../components/Audio";
 import KeyPhrases from "../components/KeyPhrases";
 import SideBar from "../components/SideBar";
 import Footer from "../components/Footer";
@@ -14,7 +14,7 @@ export default function Lesson(props) {
     <div id="lesson">
       <SideBar LessonText={props.LessonText}></SideBar>
       <TitleSection LessonText={props.LessonText}></TitleSection>
-      <Audio LessonText={props.LessonText}></Audio>
+      {/* <Audio LessonText={props.LessonText}></Audio> */}
 
       {props.LessonText.customLesson ? (
         <MainConvoCustom LessonText={props.LessonText}></MainConvoCustom>
